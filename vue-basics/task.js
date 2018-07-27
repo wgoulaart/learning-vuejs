de85fed91bdbd917f1e800d7e1f485aead63ff5c
William Goulart <williamgoulart1@hotmail.com>
2018-07-27 00:58:27 -0400

    [UPDATE] Add components with compoments

--- vue-basics/task.js
+++ vue-basics/task.js
@@ -1,3 +1,22 @@
+Vue.component('task-list', {
+  template: `
+    <div>
+      <task v-for="task in tasks" :key="task.id"> {{ task.message }} </task>
+    </div>
+  `,
+
+  data() {
+    return {
+      tasks: [
+        { message: 'Go to the store' },
+        { message: 'Go to the bank' },
+        { message: 'Go to the park '}
+      ]
+    }
+  }
+
+})
+
 Vue.component('task', {
   template: '<li> <slot></slot> </li>'
 })
