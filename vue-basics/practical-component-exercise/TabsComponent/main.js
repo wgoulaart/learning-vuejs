13bef8a311d4f209ad5ed4a42936963a6f47995e
William Goulart <williamgoulart1@hotmail.com>
2018-07-28 14:48:23 -0400

    [UPDATE] Component tabs and tab :construction:

--- vue-basics/practical-component-exercise/TabsComponent/main.js
+++ vue-basics/practical-component-exercise/TabsComponent/main.js
@@ -1,20 +1,38 @@
-Vue.component('modal', {
+Vue.component("tabs", {
   template: `
-    <div class="modal is-active">
-      <div class="modal-background"></div>
-      <div class="modal-content">
-        <div class="box">
-          <slot></slot>
-        </div>
-      </div>
-      <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+  <div>
+    <div class="tabs">
+      <ul>
+        <li v-for="tab in tabs" :class="{ 'is-active': tab.selected }" > <a href="#"> {{ tab.name }} </a> </li>
+      </ul>
     </div>
-  `
+
+    <div class="tabs-datails">
+      <slot></slot>
+    </div>
+  </div>
+  `,
+
+  data() {
+    return {
+      tabs: []
+    }
+  },
+
+  created() {
+    this.tabs = this.$children
+  }
+})
+
+Vue.component("tab", {
+  template: `<div> <slot></slot> </div>`,
+  
+  props: {
+    name: { required: true },
+    selected: { default: false}
+  }
 })
 
 new Vue({
-  el: "#root",
-  data: {
-    showModal: false
-  }
+  el: "#root"
 })
